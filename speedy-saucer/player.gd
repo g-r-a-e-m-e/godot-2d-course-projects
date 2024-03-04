9d59d1407302376dcbffd2cf7dce62a8c791d217
graeme benson <62527977+g-r-a-e-m-e@users.noreply.github.com>
2024-03-03 22:15:18 -0500

    complete Vector2 and apply_impulse lecture

--- speedy-saucer/player.gd
+++ speedy-saucer/player.gd
@@ -2,8 +2,4 @@ extends RigidBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+	apply_impulse(Vector2(25, 0))
